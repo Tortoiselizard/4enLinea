7fcf1bb35d45a9c5ad36a5ed509ecacaaafb02da
Troy Pernía <troy00pernia@gmail.com>
2023-06-05 16:01:02 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import Board from './components/Board/Board'
+
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <Board />
+    </>
+  )
+}
+
+export default App
