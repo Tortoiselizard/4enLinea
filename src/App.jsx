414c997645120eea080c6ced82797293ec4d4b9c
Troy Pernía <troy00pernia@gmail.com>
2023-06-10 17:27:07 -0400

    change src/App: created states: "boardState" (line 10) and "turn" (line 12). And add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
 import Board from './components/Board/Board'
+import Footer from './components/Footer/Footer'
+import { board, players } from './constants'
 
 import './App.css'
 
 function App () {
+  const [boardState, setBoardState] = useState(board)
+
+  const [turn, setTurn] = useState(players.player1)
+
   return (
     <>
       <header>Encabezado</header>
-      <Board />
-      <footer>Pie de página</footer>
+      <Board board={boardState} changeBoard={setBoardState} turn={turn} changeTurn={setTurn} />
+      <Footer turn={turn} />
     </>
   )
 }
