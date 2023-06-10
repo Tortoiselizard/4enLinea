d3f564865cf8c79c20963b479f10ecb5750b5d90
Troy Pernía <troy00pernia@gmail.com>
2023-06-10 16:31:16 -0400

    change src/constants: add onClick evento to button (line 6) and prop "changeboard" (line 3)

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,9 +1,9 @@
 import './Box.css'
 
-function Box ({ color, changeBoard, index }) {
+function Box ({ color, changeBoard, column }) {
   return (
     <div>
-      <button className={color === 'red' ? 'box-container-p1' : color === 'yellow' ? 'box-container-p2' : 'box-container'} onClick={() => { changeBoard(index) }} />
+      <button onClick={() => changeBoard(column)} className='box-container' id={color === 'red' ? 'box-container-p1' : color === 'yellow' ? 'box-container-p2' : 'null'} />
     </div>
   )
 }
