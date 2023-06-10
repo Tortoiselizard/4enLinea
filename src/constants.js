3ec4bcfa862a63db7b381383dc751a4147d3c4aa
Troy Pernía <troy00pernia@gmail.com>
2023-06-10 13:36:49 -0400

    change src/constants: add const "players" (line 2-11)

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,11 @@
 export const board = Array(42).fill(null)
+export const players = {
+  player1: {
+    name: 'Troy',
+    color: 'red'
+  },
+  player2: {
+    name: 'Marina',
+    color: 'yellow'
+  }
+}
