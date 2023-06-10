6fa0a6d74f803d7b930f58c01d6a0322bdc510b7
Troy Pernía <troy00pernia@gmail.com>
2023-06-10 13:34:15 -0400

    change components/Board: add new states: "boardState" (line 8), "turn" (line 10). And functions: "handleBoard" (line 12-21), "handleTurn" (line 23).

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react'
 import './Board.css'
 
 import Box from '../Box/Box'
-import { board } from '../../constants'
+import { board, players } from '../../constants'
 
 function Board () {
+  const [boardState, setBoardState] = useState(board)
+
+  const [turn, setTurn] = useState(players.player1)
+
+  function handleBoard (index) {
+    if (boardState[index] === null) {
+      setBoardState(boardState => {
+        const newBoard = [...boardState]
+        newBoard[index] = turn.color
+        return newBoard
+      })
+      handleTurn()
+    }
+  }
+
+  function handleTurn () {
+    setTurn(turn => turn.color === 'red' ? players.player2 : players.player1)
+  }
+
   return (
     <section className='board-container'>
       {
-        board.map((box, index) => <Box color={null} key={index} />)
+        boardState.map((box, index) => <Box color={box} changeBoard={handleBoard} index={index} key={index} />)
       }
     </section>
   )
