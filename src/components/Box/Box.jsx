64e7fc342f26e9ae93c7cd34ac936862f91c222b
Troy Pernía <troy00pernia@gmail.com>
2023-06-10 13:35:49 -0400

    change components/Box: add button tag to div (line 6)

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,8 +1,10 @@
 import './Box.css'
 
-function Box ({ color }) {
+function Box ({ color, changeBoard, index }) {
   return (
-    <div className={color === 'red' ? 'box-container-p1' : color === 'yellow' ? 'box-container-p2' : 'box-container'} />
+    <div>
+      <button className={color === 'red' ? 'box-container-p1' : color === 'yellow' ? 'box-container-p2' : 'box-container'} onClick={() => { changeBoard(index) }} />
+    </div>
   )
 }
 
