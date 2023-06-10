f3573d7573205eb39e9e04a84edd93a1a89abee2
Troy Pernía <troy00pernia@gmail.com>
2023-06-10 16:29:16 -0400

    change src/constants: add function placeTab (line 13-24)

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const board = Array(42).fill(null)
+export const board = Array(7).fill(null).map(() => Array(7).fill(null))
 export const players = {
   player1: {
     name: 'Troy',
@@ -8,4 +8,21 @@ export const players = {
     name: 'Marina',
     color: 'yellow'
   }
+}
+
+export function placeTab (board, column, color) {
+  const newBoard = [...board]
+  for (const index in newBoard[column]) {
+    // console.log(`ciclo ${index}:`)
+    if (newBoard[column][index] === null) {
+      console.log(`entre al if en la iteración ${index}`)
+      newBoard[column][index] = color
+      break
+    }
+  }
+  return newBoard
+}
+
+export function calculateColumn (index) {
+
 }
