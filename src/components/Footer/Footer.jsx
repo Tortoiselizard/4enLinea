28fd2aab579c6c18703f7f708763a03ca73fdae9
Troy Pernía <troy00pernia@gmail.com>
2023-06-11 15:07:20 -0400

    change components/Footer: change strign to comparate the color from "red" and "yellow" to "r" and "y" (line 8, 12)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,11 +5,11 @@ import { players } from '../../constants'
 function Footer ({ turn }) {
   return (
     <footer className='footer-container'>
-      <div id={turn.color === 'red' ? 'player-selected' : null}>
+      <div id={turn.color === 'r' ? 'player-selected' : null}>
         <span>{players.player1.name}</span>
         <label className='players-color' id='player1-color' />
       </div>
-      <div id={turn.color === 'yellow' ? 'player-selected' : null}>
+      <div id={turn.color === 'y' ? 'player-selected' : null}>
         <span>{players.player2.name}</span>
         <label className='players-color' id='player2-color' />
       </div>
