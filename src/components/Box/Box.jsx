243424510b12b993b2b62852485788a7f862658e
Troy Pernía <troy00pernia@gmail.com>
2023-06-11 15:01:48 -0400

    change components/Box: change return from component (line 6)

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -3,7 +3,7 @@ import './Box.css'
 function Box ({ color, changeBoard, column }) {
   return (
     <div>
-      <button onClick={() => changeBoard(column)} className='box-container' id={color === 'red' ? 'box-container-p1' : color === 'yellow' ? 'box-container-p2' : 'null'} />
+      <button onClick={() => changeBoard(column)} className='box-container' id={color === 'r' ? 'box-container-p1' : color === 'y' ? 'box-container-p2' : 'null'} />
     </div>
   )
 }
