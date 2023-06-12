9dfa8f07c04f0e9fa1a2a281a53400fe76ef65d8
Troy Pernía <troy00pernia@gmail.com>
2023-06-11 21:08:23 -0400

    add components/Message

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,18 @@
+import './Message.css'
+
+function Message ({ gameCondition, utility }) {
+  if (gameCondition.status === 'continue') return null
+  return (
+    <section className='alert-message'>
+      <div className='alert-message-container'>
+        <h2>{`${gameCondition.player.name} win!`}</h2>
+        <header>{gameCondition.comment}</header>
+        <footer>
+          <button onClick={() => utility()}>New Game</button>
+        </footer>
+      </div>
+    </section>
+  )
+}
+
+export default Message
