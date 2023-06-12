d49103ff52c38119a3f736385b475dc2ae055ced
Troy Pernía <troy00pernia@gmail.com>
2023-06-11 22:32:47 -0400

    add components/Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header () {
+  return (
+    <header className='header-container'>
+      <div>
+        <h1>4 en Linea</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
